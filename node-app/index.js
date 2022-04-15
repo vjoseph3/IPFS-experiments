c617cc57cbd525152961846f851d68464d832c14
Joseph Vendryes <79129654+vjoseph3@users.noreply.github.com>
2022-04-15 17:21:22 -0400

    Add and cat successfully

--- node-app/index.js
+++ node-app/index.js
@@ -1,3 +1,14 @@
-import * as IPFS from 'ipfs-core'
+import * as IPFS from "ipfs-core";
+import toBuffer from "it-to-buffer";
 
-console.log(Object.keys(IPFS));
+const ipfs = await IPFS.create({ offline: true, start: true });
+
+const { cid } = await ipfs.add("Hello, RISE!");
+console.log(cid);
+
+const helloWorldCid = "QmWGeRAEgtsHW3ec7U4qW2CyVy7eA2mFRVbk1nb24jFyks";
+const bufferedContents = await toBuffer(ipfs.cat(helloWorldCid));
+const stringContents = Buffer.from(bufferedContents.buffer).toString();
+console.log(stringContents);
+
+await ipfs.stop();
